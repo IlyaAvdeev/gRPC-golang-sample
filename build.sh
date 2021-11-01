92ff153ecd91296f4fb30f4e6d1fdc8c59aaf18d
Ilya Avdeev <avdeeff.ilya@gmail.com>
2021-11-01 20:13:32 +0300

    version updated

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./def.proto
 #protoc -I .  def.proto --go_out=plugins=grpc:.
 
-version=1.0.0
+version=1.0.1
 image_name=ilyaavdeev/grpc-server-sample
 
 sudo docker build -t $image_name:$version .
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=1.0.0
+version=1.0.1
 image_name=ilyaavdeev/grpc-server-sample
 
 sudo docker run --tty --rm -p 8080:50051 $image_name:$version
