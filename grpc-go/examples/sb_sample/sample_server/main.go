341e82fef5df909ac18f827565b22669da2dd47d
Ilya Avdeev <avdeeff.ilya@gmail.com>
2021-11-15 22:56:25 +0300

    Initial sample complete

--- grpc-go/examples/sb_sample/sample_server/main.go
+++ grpc-go/examples/sb_sample/sample_server/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"log"
 	"net"
-	//"math/rand"
+	"math/rand"
+	"github.com/golang/protobuf/jsonpb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"bytes"
 
 	"google.golang.org/grpc"
 	pb "sb_sample/sample"
@@ -18,14 +22,26 @@ type server struct {
 	pb.UnimplementedWizardServer
 }
 
+//http://www.inanzzz.com/index.php/post/yskn/handling-a-complex-json-request-within-a-grpc-client-and-server-golang-application
 func (s *server) WhatIsAgeOf(ctx context.Context, in *pb.UserName) (*pb.UserInfo, error) {
+
+	json := bytes.Buffer{}
+	// OrigName uses the actual field names from the proto files rather than casting them to camelCase.
+	// EmitDefaults prevents discarding empty/nullable fields and keeps zero values.
+	mars := jsonpb.Marshaler{OrigName: true, EmitDefaults: true}
+	if err := mars.Marshal(&json, in); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "server: marshal: %v", err)
+	}
+	log.Println("REQUEST:", json.String())
+
+
 	log.Printf("Received: %v", in.GetName())
-	//return &pb.WhatIsAgeOf{Message: "Hello " + in.GetName(), Age: rand.Intn(10)}
-	
+	rand.NewSource(100)
 	user_info := &pb.UserInfo {
 		Message: in.GetName(),
-		Age:   35,
+		Age:   uint32(rand.Intn(100)),
 	}
+	
 	return user_info, nil
 }
 
